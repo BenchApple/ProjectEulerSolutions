7171ac157909d931560e9397f3dba556ccd5dbe0
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-11-16 13:25:03 -0500

    Update CircularPrimes.py

--- CircularPrimes.py
+++ CircularPrimes.py
@@ -1,4 +1,5 @@
 #Benjamin Chappell
+#Benjamin Chappell
 
 def main():
 	limit = 1000000
@@ -9,9 +10,36 @@ def main():
 	
 	primes = seive(primes, limit)
 	
-def seive(a, l):
-	for i in range(2, l ** (1/2)):
+	
+	
+def seive(a, n): #Implementation of the Seive of Eratosthenes
+	for i in range(2, n ** (1/2)):
+		if a[i]:
+			for j in range(i ** 2, n):
+				a[j] = False
+	
+	toReturn = []
+	
+	for i in range(0, len(primes)):
+		if a[i]:
+			toReturn.append(i)
+	
+def search(a, n): #Implementation of Binary search, shell method for easy calls
+	search(a, n, 0, len(a) - 1)
+	
+	
+def search(a, n, start, end): #Underlying method for search.
+	if (start > end):
+		return False
+	else:
+		m = a[(start + end) / 2]
 		
+		if (m = n):
+			return True
+		else if m > 
+			
+			
+	
 	
 if __name__ == "__main__":
 	main()
