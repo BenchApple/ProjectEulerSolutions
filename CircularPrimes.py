7d907039e6adb4c86ba52ffc7797d72efb2a8176
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-11-16 22:13:00 -0500

    getting closer

--- CircularPrimes.py
+++ CircularPrimes.py
@@ -1,16 +1,30 @@
 #Benjamin Chappell
-#Benjamin Chappell
+
+from itertools import permutations
 
 def main():
-	limit = 1000000
-	
-	primes = [] #index 0 is equal to 2 in the real world, make sure to account for that transition.
-	for i in range(0, limit - 2):
+	limit, counter = 1000000
+
+    primes = [] #index 0 is equal to 2 in the real world, make sure to account for that transition.
+    
+    for i in range(0, limit - 2):
 		primes[i] = True
 	
 	primes = seive(primes, limit)
-	
-	
+
+    toPermute = []
+	for i in primes:
+        toPermute = list(str(i))
+        isCircular = True
+
+        for i in permutations(toPermute):
+            if not search(primes, int("".join(i)):
+                isCircular = False
+                break
+        
+        if isCircular:
+        counter += 1
+
 	
 def seive(a, n): #Implementation of the Seive of Eratosthenes
 	for i in range(2, n ** (1/2)):
@@ -23,6 +37,8 @@ def seive(a, n): #Implementation of the Seive of Eratosthenes
 	for i in range(0, len(primes)):
 		if a[i]:
 			toReturn.append(i)
+
+    return toReturn
 	
 def search(a, n): #Implementation of Binary search, shell method for easy calls
 	search(a, n, 0, len(a) - 1)
@@ -34,11 +50,12 @@ def search(a, n, start, end): #Underlying method for search.
 	else:
 		m = a[(start + end) / 2]
 		
-		if (m = n):
+		if m = n:
 			return True
-		else if m > 
-			
-			
+		elif m > n:
+            return search(a, n, m+1, end)
+        elif m < n:
+            return search(a, n, start, m-1)
 	
 	
 if __name__ == "__main__":
