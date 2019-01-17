688f1254fe081c1b9cbef41a1951824c5bffd830
ImDaNoob <bjchappell02@icloud.com>
2019-01-17 14:31:32 -0500

    added a little to pandigital

--- PandigitalPrime.py
+++ PandigitalPrime.py
@@ -1,7 +1,18 @@
 """ Benjamin Chappell """
 
-def main():
+def main():   
+    primes = list(findPrimes(9876543210))
     numberCheck = []
+
+
+def findPrimes(n): #Returns a list of primes below n
+	sieve = [True] * n
+
+	for i in range(3, int(n**0.5) + 1, 2):
+		if sieve[i]:
+			sieve[i*i :: 2*i] = [False]*((n-i*i-1)/(2*i)+1)
+
+	return [2] + [i for i in range(3, n, 2) if sieve[i]]
 
 if __name__ == "__main__":
     main()
